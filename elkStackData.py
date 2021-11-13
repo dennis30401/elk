a1e80e227276a3ea375e98e832659296203d1bee
dennis30401 <56074426+dennis30401@users.noreply.github.com>
2021-11-13 22:28:29 +0800

    Create elkStackData.py

---
+++ elkStackData.py
@@ -0,0 +1,100 @@
+import requests
+import json
+import time
+from elasticsearch import Elasticsearch
+import schedule
+import random
+from datetime import datetime, timezone, timedelta
+
+def get_humidity():
+    # humidity_URL = "http://192.168.0.16/things/temp-hum-light-1/properties"
+    # response = requests.get(humidity_URL)
+    # property_text = response.text
+    # property_json = json.loads(property_text)
+    # print("humidity:" , property_json["hum"],end=' ')
+    # print("light:" , property_json["light"],end=' ')
+    # print("temp:" , property_json["temp"])
+    hum = random.randint(0,100)
+    return hum
+
+def get_people():
+    # people_URL = "http://127.0.0.1:6666/properties"
+    # response = requests.get(people_URL)
+    # property_text = response.text
+    # property_json = json.loads(property_text)
+    # print("number:",property_json["amount"])
+    # return property_json["amount"]
+    hum = random.randint(0,100)
+    return hum
+
+def get_datetime():   # get UTC+8 time format
+    """ return formatted time(YYYY-MM-DD HH:MM:SS UTC+8) """
+    # set time zone(TW->UTC+8)
+    tz = timezone(timedelta(hours=+8))
+    # get the current time、 time zone, and convert to ISO format(to second) e.g. "2021-09-19T16:50:00+08:00"
+    gltime = datetime.now(tz).isoformat(timespec="seconds")
+    return gltime
+
+def send_number_to_elastucSearch(es):
+    fmDate = get_datetime()
+    peopleNumber = get_people()
+    datas = {
+        "time":fmDate,
+        "PeopleNum":peopleNumber
+    }
+    print("number:",peopleNumber,type(peopleNumber),fmDate)
+    # es.index(index='opentest',body = datas)
+
+def send_humidity_to_elastucSearch(es):
+    fmDate = get_datetime()
+    humidity = get_humidity()
+    datas = {
+        "time":fmDate,
+        "humidity":humidity
+    }
+    print("hum:",humidity,type(humidity),fmDate)
+    # es.index(index='humtest',body = datas)
+
+def multipleSchedulers():
+    """ Manually set the tasks you want to schedule """
+    es = Elasticsearch(hosts='127.0.0.1', port=9200)
+    scheduler1 = schedule.Scheduler()  # to send people number to es 
+    scheduler2 = schedule.Scheduler()  # to send humidity to es
+    scheduler1.every().hour.at(":00").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":05").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":10").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":15").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":20").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":25").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":30").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":35").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":40").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":45").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":50").do(send_number_to_elastucSearch,es)
+    scheduler1.every().hour.at(":55").do(send_number_to_elastucSearch,es)
+
+    scheduler2.every().hour.at(":00").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":05").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":10").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":15").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":20").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":25").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":30").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":35").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":40").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":45").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":50").do(send_humidity_to_elastucSearch,es)
+    scheduler2.every().hour.at(":55").do(send_humidity_to_elastucSearch,es)
+
+    while True:
+        scheduler1.run_pending()
+        scheduler2.run_pending()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    print("start scheduler")
+    # multipleSchedulers()
+    # no scheduler test
+    es = Elasticsearch(hosts='140.134.25.64', port=19200)
+    send_humidity_to_elastucSearch(es)
+    send_number_to_elastucSearch(es)
